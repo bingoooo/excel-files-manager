da51028551d2e92edb5068374f5f0aec095b01b4
Benjamin Dant <benjamindant@gmail.com>
2016-07-20 12:52:20 +0200

    Ajout de la bibliothèque exceljs et test d'intégration sur une page électron

--- renderer.js
+++ renderer.js
@@ -1,3 +1,21 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+'use strict';
+
+var Excel = require('exceljs');
+
+console.log('sourced : renderer.js');
+
+var workbook = new Excel.Workbook();
+var Create = document.getElementById('create');
+Create.addEventListener('click', ()=>{
+	console.log('creating ')
+	workbook.creator = 'Bingoooo';
+	workbook.lastModifiedBy = 'Bingoooo';
+	workbook.created = new Date();
+	workbook.modified = new Date();
+	var Infos =  document.getElementById('infos-field');
+	Infos.innerHTML = 'New File ' + workbook.creator + ' Created ' + workbook.created;
+});
+console.log(workbook);
