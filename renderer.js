d3299d5265a78054e4423a594f664896bb487fe8
Benjamin Dant <benjamindant@gmail.com>
2016-07-21 16:58:02 +0200

    Added toggle hide functionfor the modal

--- renderer.js
+++ renderer.js
@@ -5,19 +5,31 @@
 
 var Excel = require('exceljs');
 var $ = require('jquery');
+var fs = require('fs');
 
 console.log('sourced : renderer.js');
 
+// Création d'un objet excel
 var workbook = new Excel.Workbook();
 var Create = document.getElementById('create');
+var Infos =  document.getElementById('infos-field');
 Create.addEventListener('click', ()=>{
 	console.log('creating ')
 	workbook.creator = 'Bingoooo';
 	workbook.lastModifiedBy = 'Bingoooo';
 	workbook.created = new Date();
 	workbook.modified = new Date();
-	var Infos =  document.getElementById('infos-field');
 	Infos.innerHTML = 'New File ' + workbook.creator + ' Created ' + workbook.created;
 });
+
+// Animation de la page de crédits
+var Credits = document.getElementById('credits');
+Credits.addEventListener('click', ()=>{
+	var About = document.getElementById('about');
+	About.classList.toggle('hide'); 
+});
+
+// TODO : Utilisation du file system pour la gestion de fichiers
+
 console.log($('create'));
 console.log(workbook);
