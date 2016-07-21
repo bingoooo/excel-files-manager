dd44ede45053da709762483a3a4385a225671559
Benjamin Dant <benjamindant@gmail.com>
2016-07-21 19:01:22 +0200

    Added winow onload function, Modal Boxes management

--- renderer.js
+++ renderer.js
@@ -9,27 +9,41 @@ var fs = require('fs');
 
 console.log('sourced : renderer.js');
 
-// Création d'un objet excel
-var workbook = new Excel.Workbook();
-var Create = document.getElementById('create');
-var Infos =  document.getElementById('infos-field');
-Create.addEventListener('click', ()=>{
-	console.log('creating ')
-	workbook.creator = 'Bingoooo';
-	workbook.lastModifiedBy = 'Bingoooo';
-	workbook.created = new Date();
-	workbook.modified = new Date();
-	Infos.innerHTML = 'New File ' + workbook.creator + ' Created ' + workbook.created;
-});
 
-// Animation de la page de crédits
-var Credits = document.getElementById('credits');
-Credits.addEventListener('click', ()=>{
-	var About = document.getElementById('about');
-	About.classList.toggle('hide'); 
-});
+window.onload = function(){
 
-// TODO : Utilisation du file system pour la gestion de fichiers
+	// Apparition de la modal de création de fichier
+	var NewFile = document.getElementById('new');
+	NewFile.addEventListener('click', ()=>{
+		console.log('New File');
+		var Modal = document.getElementById('new-file-modal');
+		Modal.classList.toggle('hide');
+	})
 
-console.log($('create'));
-console.log(workbook);
+	// Création d'un objet excel
+	var workbook = new Excel.Workbook();
+	var Create = document.getElementById('create');
+	var Infos =  document.getElementById('infos-field');
+	Create.addEventListener('click', ()=>{
+		var filename = document.getElementById('filename').value;
+		workbook.creator = 'Bingoooo';
+		workbook.lastModifiedBy = 'Bingoooo';
+		workbook.created = new Date();
+		workbook.modified = new Date();
+		Infos.innerHTML = 'New File ' + filename + ' Created at ' + workbook.created;
+	});
+
+	// Animation de la page de crédits
+	var Credits = document.getElementById('credits');
+	Credits.addEventListener('click', ()=>{
+		var Modal = document.getElementById('credits-modal');
+		Modal.classList.toggle('hide'); 
+	});
+
+	// TODO : Utilisation du file system pour la gestion de fichiers
+
+	console.log($('create'));
+	// console.log(workbook);
+
+
+}
