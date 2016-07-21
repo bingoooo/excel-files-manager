ebf23e5f0fc5c8aac455a1f11e7c8b468adca0f6
Benjamin Dant <benjamindant@gmail.com>
2016-07-21 19:02:02 +0200

    Added class for react Cell

--- source/App.js
+++ source/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Cell extends Component {
 	render(){
 		return (
-			<h1>My First Cell</h1>
+			<h1 className="bd-cell hide">My First Cell</h1>
 		)
 	}
 }
